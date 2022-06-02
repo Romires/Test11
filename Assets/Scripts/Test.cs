061811ee79213822d0262a383588d13fe64484b7
Romires <qsort@yandex.ru>
2022-06-02 20:06:35 +0300

    lesson1

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class Test : MonoBehaviour
+    {
+        class EmailAddress
+        {
+            public string Name { get; }
+            public string Address { get; }
+            public EmailAddress(string n, string a)
+            {
+                Name = n;
+                Address = a;
+            }
+        }
+
+        private void Start()
+        {
+            EmailAddress[] addrs = { 
+                new EmailAddress("Herb", "Herb@HerbSchildt.com"), 
+                new EmailAddress("Tom", "Tom@HerbSchildt.com"), 
+                new EmailAddress("Sara", "Sara@HerbSchildt.com") 
+            };
+            // Create a query that selects e-mail addresses
+            var eAddrs = from entry in addrs
+                select entry.Address;
+            Debug.Log("The e-mail addresses are");
+            // Execute the query and display the results
+            foreach (string s in eAddrs)
+            {
+                Debug.Log("  " + s);
+            }
+        }
+    }
+}
